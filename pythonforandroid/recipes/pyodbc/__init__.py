7695b6c0f2dce6c26719befd51a40b03b07dbf6b
Arnulfo Rojas <48840058+drarn96@users.noreply.github.com>
2020-04-26 15:34:33 -0500

    Update __init__.py

--- pythonforandroid/recipes/pyodbc/__init__.py
+++ pythonforandroid/recipes/pyodbc/__init__.py
@@ -8,7 +8,7 @@ class PyodbcRecipe(CompiledComponentsPythonRecipe):
     version = '4.0.30'
     url = 'https://github.com/mkleehammer/pyodbc/archive/{version}.tar.gz'
     site_packages_name = 'pyodbc'
-    depends = ['setuptools']
+    depends = ['setuptools','odbc']
     
     call_hostpython_via_targetpython = False
     install_in_hostpython = False
