8210b0e2ccc752bedcfb88e1805201ab18a377fd
Arnulfo Rojas <48840058+drarn96@users.noreply.github.com>
2020-04-25 22:08:10 -0500

    Update __init__.py

--- pythonforandroid/recipes/pyodbc/__init__.py
+++ pythonforandroid/recipes/pyodbc/__init__.py
@@ -5,7 +5,7 @@ class PyodbcRecipe(CompiledComponentsPythonRecipe):
     version = '4.0.30'
     url = 'https://github.com/mkleehammer/pyodbc/archive/{version}.tar.gz'
     site_packages_name = 'pyodbc'
-    #depends = ['python3']
+    depends = ['python3','python-dev-tools']
     #conflicts = []
 
 
